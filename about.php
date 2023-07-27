b870dc98ce2ff641d216a9feebfda1273ba60f5b
Fabian Herrera Martinez <95977433+FabianHMzz@users.noreply.github.com>
2023-07-27 01:09:24 -0600

    ✨ About section finished

---
+++ about.php
@@ -0,0 +1,287 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Nosotros</title>
+  <link rel="shortcut icon" type="image/x-icon" href="static/favicon/favicon.ico" />
+  <meta name="viewport" content="width=device-width" />
+  <link rel="apple-touch-icon" sizes="180x180" href="static/favicon/apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="static/favicon/favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="static/favicon/favicon-16x16.png" />
+  <link rel="manifest" href="static/favicon/site.webmanifest" />
+  <link rel="mask-icon" href="static/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+  <meta name="description" content="Proffesional Cleaning, Professional Care" />
+  <meta name="msapplication-TileColor" content="#ffffff" />
+  <meta name="theme-color" content="#ffffff" />
+  <meta name="apple-mobile-web-app-title" content="Laundry Miss Clean" />
+  <meta name="application-name" content="Laundry Miss Clean" />
+  <meta property="og:title" content="Laundry Miss Clean" />
+  <meta property="og:description" content="Professional Cleaning, Professional Care" />
+  <meta property="og:locate" content="es_MX" />
+  <meta property="og:site_name" content="Laundry Miss Clean" />
+  <link rel="stylesheet" href="/CSS/about.css">nk
+</head>
+
+<body>
+  <header class="header">
+    <nav>
+      <div class="logo">
+        <img src="static/svg/MCL.svg" alt="Miss Laundry Logo" width="100">
+      </div>
+      <input type="checkbox" id="menu-toggle">
+      <label for="menu-toggle" class="menu-icon">&#9776;</label>
+      <ul class="menu">
+        <li><a href="/">Inicio</a></li>
+        <li><a href="/about.php">Nosotros</a></li>
+        <li><a href="/services.php">Servicios</a></li>
+        <li><a href="/contact.php">Contacto</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <section class='services2' id='home'>
+    <div class='servicesblack2'>
+      <div class='hero-liYrbf'>
+        <div class='hero-EmJyBo'>
+          <div class='hero-3uQi3N'>
+            <div class='hero-gDmIjy'>
+              <div class='hero-g5HyBX'>
+                <p class='hero-Wdo2GU'>
+                  Acerca de nosotros
+                </p>
+                <footer class="hero-ZhC6gs" aria-label="Breadcrumb">
+                  <ul class="hero-0o3ugA">
+                    <li class="hero-cKBPVH">
+                      <a href="/" class="hero-zSisTE">
+                        <svg aria-hidden="true" class="hero-ho5j6b" fill="currentColor" viewBox="0 0 20 20"
+                          xmlns="http://www.w3.org/2000/svg">
+                          <path
+                            d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z">
+                          </path>
+                        </svg>
+                        Home
+                      </a>
+                    </li>
+                    <li>
+                      <div class="hero-wvSXiL">
+                        <svg aria-hidden="true" class="hero-aW0tU0" fill="currentColor" viewBox="0 0 20 20"
+                          xmlns="http://www.w3.org/2000/svg">
+                          <path fill-rule="evenodd"
+                            d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                            clip-rule="evenodd"></path>
+                        </svg>
+                        <a href="/about" class="hero-6e6ERm">Acerca de nosotros</a>
+                      </div>
+                    </li>
+                  </ul>
+                </footer>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section>
+    <article>
+      <div class="mission-X3lw5m">
+        <div class="mission-WfvN53">
+          <div class="mission-NbSIoN">
+            <img alt="Mision"
+              src="https://images.unsplash.com/photo-1635369306367-9d891c6c1a7c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+              class="mission-wIr0p5" />
+          </div>
+
+          <div class="mission-tv8qtZ">
+            <h2 class="mission-yYqqxd">Misión</h2>
+
+            <p class="mission-ENav7D">
+              Nuestra misión es proporcionar un servicio de lavado de ropa excepcional y conveniente para nuestros
+              clientes.
+              Nos esforzamos por cuidar de su ropa de manera eficiente y brindarles una experiencia satisfactoria. Nos
+              comprometemos a
+              utilizar tecnología avanzada y prácticas sostenibles para minimizar nuestro impacto en el medio ambiente.
+              Nuestra meta es simplificar el
+              cuidado de la ropa y mejorar la vida de las personas a través de nuestro servicio confiable y amigable.
+            </p>
+          </div>
+        </div>
+      </div>
+    </article>
+    <article>
+      <div class="vision-kpz4GN">
+
+        <div class="vision-Xrjimm">
+          <div class="vision-yj0XDN">
+            <img alt="Vision"
+              src="https://images.unsplash.com/photo-1508759073847-9ca702cec7d2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+              class="vision-TcNDX6" />
+          </div>
+
+          <div class="vision-LzFTeL">
+            <h2 class="vision-Ps3NqO">Visión</h2>
+            <p class="vision-r4VyCn">
+              Nuestra lavandería se esfuerza por brindar a los clientes una experiencia de lavado y secado de ropa
+              rápida, conveniente y de calidad.
+              Nos destacamos por nuestra tecnología avanzada, enfoque en la sostenibilidad y servicio excepcional.
+              Queremos simplificar la vida de las
+              personas al ofrecer un lugar limpio y acogedor, con máquinas intuitivas y amigables. Nuestro objetivo es
+              ser la lavandería preferida, donde los
+              clientes confíen en que su ropa será cuidada de manera eficiente y respetuosa con el medio ambiente.
+            </p>
+          </div>
+        </div>
+      </div>
+    </article>
+    <article class="mission-X3lw5m">
+      <div class="mission-WfvN53">
+        <div class="mission-NbSIoN">
+          <img alt="Mision"
+            src="https://images.unsplash.com/photo-1650964336783-fd8c0c241b13?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2069&q=80"
+            class="mission-wIr0p5" />
+        </div>
+
+        <div class="mission-tv8qtZ">
+          <h2 class="mission-yYqqxd">Valores Organizacionales</h2>
+
+          <ul>
+            <li class="mission-4YmbVP">1.- Calidad
+              <p class="mission-ptWQFV">
+                Nos comprometemos a ofrecer un servicio de calidad excepcional en todas nuestras operaciones.
+                Nos esforzamos por cuidar de la ropa de nuestros clientes con atención meticulosa y garantizar
+                resultados impecables en cada lavado.
+              </p>
+            </li>
+            <li class="mission-4YmbVP">2.- Confiabilidad
+              <p class="mission-ptWQFV">
+                Valoramos la confianza de nuestros clientes y nos esforzamos por ser un servicio en el que puedan
+                confiar plenamente.
+                Cumplimos con nuestros compromisos, ofreciendo tiempos de entrega confiables y garantizando la seguridad
+                y el cuidado
+                de la ropa de nuestros clientes.
+              </p>
+            </li>
+            <li class="mission-4YmbVP">3.- Responsabilidad
+              <p class="mission-ptWQFV">
+                Reconocemos nuestra responsabilidad hacia nuestros clientes, empleados y el medio ambiente.
+                Nos comprometemos a operar de manera ética y sostenible, minimizando nuestro impacto en el entorno
+                y tomando decisiones responsables en todas nuestras actividades.
+              </p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </article>
+    <div class="vision-kpz4GN">
+
+      <div class="vision-Xrjimm">
+        <div class="vision-yj0XDN">
+          <img alt="Vision"
+            src="https://images.unsplash.com/photo-1549037173-e3b717902c57?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+            class="vision-TcNDX6" />
+        </div>
+
+        <div class="vision-LzFTeL">
+          <h2 class="vision-Ps3NqO">Política de Calidad</h2>
+          <p class="vision-r4VyCn">
+            En nuestra lavandería, nos comprometemos a brindar un servicio excepcional y cuidar de la ropa de nuestros
+            clientes de manera óptima.
+            Nos esforzamos por cumplir con los estándares de calidad, mejorar continuamente nuestros procesos y operar
+            de manera sostenible.
+            Valoramos a nuestro equipo y su desarrollo profesional. Nuestro objetivo es garantizar la satisfacción de
+            nuestros clientes y mantener
+            un ambiente seguro y limpio en nuestras instalaciones.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <footer class='footer-9XOR03'>
+    <div class='footer-xlE1cw'>
+      <div class='footer-YYWlWo'>
+        <div class='footer-7GIQa3'>
+          <img src='/static/svg/MCL.svg' class='footer-8eXE9L' alt='Laundry Miss Clean Logo' />
+        </div>
+        <div class='footer-loQjmt'>
+          <div>
+            <h2 class='footer-oTja34'>
+              Servicios
+            </h2>
+            <ul class='footer-x4jPXX'>
+              <li class='footer-bzqNFA'>
+                <a href='/services' class='footer-tCldzq'>
+                  Lavandería
+                </a>
+              </li>
+              <li class='footer-bzqNFA'>
+                <a href='/services' class='footer-tCldzq'>
+                  Tintorería
+                </a>
+              </li>
+              <li class='footer-bzqNFA'>
+                <a href='/services' class='footer-tCldzq'>
+                  Limpieza de Alfombras
+                </a>
+              </li>
+              <li class='footer-bzqNFA'>
+                <a href='/services' class='footer-tCldzq'>
+                  Limpieza de Cuero
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2 class='footer-oTja34'>
+              Siguenos
+            </h2>
+            <ul class='footer-x4jPXX'>
+              <li class='footer-bzqNFA'>
+                <a href='https://www.facebook.com' class='footer-tCldzq '>
+                  Facebook
+                </a>
+              </li>
+              <li>
+                <a href='https://www.twitter.com' class='footer-tCldzq'>
+                  Instagram
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2 class='footer-oTja34'>
+              Legal
+            </h2>
+            <ul class='footer-x4jPXX'>
+              <li class='footer-bzqNFA'>
+                <a href='https://missclean-laundry.vercel.app' class='footer-tCldzq'>
+                  Politica de Privacidad
+                </a>
+              </li>
+              <li>
+                <a href='https://missclean-laundry.vercel.app' class='footer-tCldzq'>
+                  Terminos &amp; Condiciones
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <hr class='footer-qnfHB0' />
+      <div class='footer-ApYPgx'>
+        <a href='https://fabianhmz.dev/' class='footer-wdN3JK' target='_blank' rel='noopener noreferrer'>
+          Made with ❤️ by Fabian Herrera
+        </a>
+        &nbsp; &amp; &nbsp;
+        <a class='footer-wdN3JK' href='https://boxode.org/' target='_blank' rel='noopener noreferrer'>
+          BoxodeHQ
+        </a>
+      </div>
+    </div>
+  </footer>
+</body>
+
+</html>
